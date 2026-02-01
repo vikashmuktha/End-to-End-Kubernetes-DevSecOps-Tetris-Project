a82cdc6b9208f520abfba00c53cf2ba598c2f5b0
Vikash M <111482795+vikashmuktha@users.noreply.github.com>
2026-02-01 13:45:09 +0530

    Reduce EKS node group desired and max size to 1

--- EKS-TF/eks-node-group.tf
+++ EKS-TF/eks-node-group.tf
@@ -6,8 +6,8 @@ resource "aws_eks_node_group" "eks-node-group" {
 
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 1
     min_size     = 1
   }
 
